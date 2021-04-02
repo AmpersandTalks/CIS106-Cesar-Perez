6e907cf3d111897e6eed7fd8b6eb8eaee9441ee4
Dave Braunschweig <dbraunsc@harpercollege.edu>
2021-04-02 11:56:48 -0500

    Assignment 10 corrections

--- Assignment 10/Activity 1.js
+++ Assignment 10/Activity 1.js
@@ -6,18 +6,18 @@ function main() {
     
     number = getValue("number");
     factor = getValue("factor");
-    increment = 1;
-    forLoop(number, factor, increment);
+    processExpressions(number, factor);
 }
 
-function forLoop(number, factor, increment) {
+function processExpressions(number, factor) {
+    var increment = 1;
     window.alert(" While loop multiplication from " + number + " to " + factor + " by " + increment + " : ");
     var product;
     
     product = number;
     for (increment = increment; increment <= factor; increment++) {
         product = number * increment;
-        window.alert(number.ToString() + " * " + increment + " = " + product);
+        window.alert(number.toString() + " * " + increment + " = " + product);
     }
 }
 
@@ -29,3 +29,38 @@ function getValue(name) {
     
     return value;
 }
+
+if (typeof alert === "undefined") {
+    global.alert = function(message) {
+        console.log(message);
+    };
+}
+
+if (typeof prompt === "undefined") {
+    global.prompt = function(message) {
+        var fs = require("fs");
+        var result = "";
+        var buffer = Buffer.alloc(1);
+
+        console.log(message);
+        for(;;){
+            fs.readSync(0, buffer, 0, 1);
+            if(buffer[0] === 10){
+                break;
+            }else if(buffer[0] !== 13){
+                result += buffer.toString();
+            }
+        }
+
+        return result;
+    }
+}
+
+if (typeof window === "undefined") {
+    global.window = {
+        alert : global.alert,
+        prompt : global.prompt
+    };
+}
+
+main();
