fa319aa4dabcdadb7aff8319d0f21412b177d88f
AmpersandTalks <54295557+AmpersandTalks@users.noreply.github.com>
2021-06-01 22:54:15 -0500

    Add files via upload

---
+++ Assignment 4/Activity 1.py
@@ -0,0 +1,20 @@
+# This program calculates  their weekly, monthly, and annual gross pay.
+#
+# References:
+#  https://en.wikibooks.org/wiki/JavaScript
+
+print("Enter hours worked this week : ")
+hours = float(input())
+
+print("Enter rate per hour : ")
+ratePerHour = float(input())
+
+weekly = hours * ratePerHour
+monthly = hours * ratePerHour * 4
+annual = hours * ratePerHour * 52
+
+print("$" + str(weekly) + " weekly " +
+    "$" + str(monthly) + " monthly " +
+    "$" + str(annual) + " annually ")
+
+
---
+++ Assignment 4/Activity 3.py
@@ -0,0 +1,16 @@
+# This program converts the unit miles in to yards, feet, and inches.
+#
+# Reference:
+#  https://www.mathsisfun.com/measure/us-standard-length.html
+#  https://en.wikibooks.org/wiki/JavaScript
+
+print('Enter miles distance')
+miles = float(input())
+yards = miles * 1760
+feet = miles * 5280
+inches = miles * 63360
+
+print(str(miles) + " miles is " +
+    str(yards) + " yards " +
+    str(feet) +  " feet " +
+    str(inches) +  " inches ")
