98f6b4f672fb6c349cf5b611c0809f9c2889c113
AmpersandTalks <54295557+AmpersandTalks@users.noreply.github.com>
2021-06-03 21:01:08 -0500

    Update Activity 3.py

--- Assignment 4/Activity 3.py
+++ Assignment 4/Activity 3.py
@@ -1,11 +1,10 @@
 # This program converts the unit miles in to yards, feet, and inches.
-#
-# Reference:
-#  https://www.mathsisfun.com/measure/us-standard-length.html
+# Reference:https://www.mathsisfun.com/measure/us-standard-length.html
 #  https://en.wikibooks.org/wiki/JavaScript
 
 print('Enter miles distance')
 miles = float(input())
+
 yards = miles * 1760
 feet = miles * 5280
 inches = miles * 63360
