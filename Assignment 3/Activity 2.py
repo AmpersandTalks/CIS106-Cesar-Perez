ea20ab192801c3ef4d92a5fee6595bd51fa6d196
AmpersandTalks <54295557+AmpersandTalks@users.noreply.github.com>
2021-06-03 21:02:07 -0500

    Update Activity 2.py

--- Assignment 3/Activity 2.py
+++ Assignment 3/Activity 2.py
@@ -1,8 +1,13 @@
 # This program asks for the amount of Years and converts into how many months,days,hours, and seconds.
 print("What is your age in years?")
 year = float(input())
+
 month = year * 12
 day = year * 365
 hour = year * 8760
 seconds = year * 31536000
-print(str(month) + " months " + str(day) + " days " + str(hour) + " hours " + str(seconds) + " seconds ")
+
+print(str(month) + " months " + 
+      str(day) + " days " + 
+      str(hour) + " hours " + 
+      str(seconds) + " seconds ")
