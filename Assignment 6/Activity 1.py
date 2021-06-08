75794d6180f54676bcc32a7f72918e6ae5cb0d47
AmpersandTalks <54295557+AmpersandTalks@users.noreply.github.com>
2021-06-08 16:01:04 -0500

    Update Activity 1.py

--- Assignment 6/Activity 1.py
+++ Assignment 6/Activity 1.py
@@ -1,34 +1,34 @@
 
-def calculate_monthly(hours, rate_per_hour)
+def calculate_monthly(hours, rate_per_hour):
     monthly = hours * rate_per_hour * 4
     
     return monthly
 
 
-def calculate_annual(hours, rate_per_hour) 
+def calculate_annual(hours, rate_per_hour):
     annual = hours * rate_per_hour * 52
     
     return annual
 
 
-def calculate_weekly(hours, rate_per_hour) 
+def calculate_weekly(hours, rate_per_hour):
     weekly = hours * rate_per_hour
     
     return weekly
 
 
-def Display_result(weekly, monthly, annual) 
+def Display_result(weekly, monthly, annual):
     print(Str(weekly) + " weekly " + monthly + " monthly " + annual + " annually ")
 
 
-def get_hours() 
+def get_hours():
     print("Enter hours worked this week")
     hours = float(input())
     
     return hours
 
 
-def get_rate_per_hour() 
+def get_rate_per_hour():
     print("Enter rate per hour")
     rate_per_hour = float(input())
     
