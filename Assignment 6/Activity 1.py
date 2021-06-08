d23b01e233dad50e81a8b6b704fba2e86558745f
AmpersandTalks <54295557+AmpersandTalks@users.noreply.github.com>
2021-06-08 16:38:28 -0500

    Update Activity 1.py

--- Assignment 6/Activity 1.py
+++ Assignment 6/Activity 1.py
@@ -1,18 +1,15 @@
 def calculate_monthly(hours, rate_per_hour):
     monthly = hours * rate_per_hour * 4
-    
     return monthly
 
 
 def calculate_annual(hours, rate_per_hour):
     annual = hours * rate_per_hour * 52
-    
     return annual
 
 
 def calculate_weekly(hours, rate_per_hour):
     weekly = hours * rate_per_hour
-    
     return weekly
 
 
@@ -23,19 +20,18 @@ def Display_result(weekly, monthly, annual):
 def get_hours():
     print("Enter hours worked this week")
     hours = float(input())
-    
     return hours
 
 
 def get_rate_per_hour():
     print("Enter rate per hour")
     rate_per_hour = float(input())
-    
     return rate_per_hour
 
 # This program calculates  their weekly, monthly, and annual gross pay.
 # References: https://en.wikibooks.org/wiki/JavaScript
 
+#main
 hours = get_hours()
 rate_per_hour = get_rate_per_hour()
 weekly = calculate_weekly(rate_per_hour, hours)
