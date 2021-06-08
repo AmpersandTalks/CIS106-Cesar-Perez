48c5eaf3916d4c7e55450de36f275a463fc2edf8
AmpersandTalks <54295557+AmpersandTalks@users.noreply.github.com>
2021-06-07 23:21:47 -0500

    Update Activity 2.py

--- Assignment 5/Activity 2.py
+++ Assignment 5/Activity 2.py
@@ -1,37 +1,37 @@
-def calculatedays(year):
-    days = year * 365
+def calculatedays(years):
+    days = years * 365
     
     return days
 
-def calculatehours(year):
-    hours = year * 8760
+def calculatehours(years):
+    hours = years * 8760
     
     return hours
 
-def calculatemonths(year):
-    months = year * 12
+def calculatemonths(years):
+    months = years * 12
     
     return months
 
-def calculateseconds(year):
-    seconds = year * 31536000
+def calculateseconds(years):
+    seconds = years * 31536000
     
     return seconds
 
-def displayResult(year, months, days, hours, seconds):
-    print(str(year) + " year(s) " + str(months) + " months " + str(days) + " days " + str(hours) + " hours " + str(seconds) + " seconds ")
+def displayResult(years, months, days, hours, seconds):
+    print(str(years) + " year(s) " + str(months) + " months " + str(days) + " days " + str(hours) + " hours " + str(seconds) + " seconds ")
 
-def getyear():
+def getyears():
     print("How old are you?")
-    year = float(input())
+    years = float(input())
     
-    return year
+    return years
 
 # Main
 # This program converts the unit of Years into months, days, hours, and seconds.
-year = getyear()
-months = calculatemonths(year)
-days = calculatedays(year)
-hours = calculatehours(year)
-seconds = calculateseconds(year)
-displayResult(year, months, days, hours, seconds)
+years = getyears()
+months = calculatemonths(years)
+days = calculatedays(years)
+hours = calculatehours(years)
+seconds = calculateseconds(years)
+displayResult(years, months, days, hours, seconds)
