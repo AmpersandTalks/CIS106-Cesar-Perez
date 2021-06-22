2916a70d09c90e6f4d5c3265a7afee778fe25a8c
AmpersandTalks <54295557+AmpersandTalks@users.noreply.github.com>
2021-06-22 04:24:29 -0500

    Update Activity 2.py

--- Assignment 8/Activity 2.py
+++ Assignment 8/Activity 2.py
@@ -1,28 +1,29 @@
 # this program uses a loop to generate a list of multiplication expressions for a given value
 
+
 def getAverage(totalScores, total):
     average = float(totalScores) / total
-    
     return average
+
 
 def getScores():
     print(" Enter score value: ")
     scores = int(input())
-    
     return scores
+
 
 def getTotal(scores, total, average):
     print(" The avergae of all these score is " + str(average))
 
+    
 def getTotalScores(totalScores, scores):
     sumScores = totalScores + scores
-    
     return sumScores
+
 
 def getValue(name):
     print(" Enter " + name + " value: ")
     value = int(input())
-    
     return value
 
 
@@ -32,7 +33,9 @@ def main():
     for counter in range(1, total + 1, 1):
         scores = getScores()
         totalScores = getTotalScores(totalScores, scores)
+        
     average = getAverage(totalScores, total)
     getTotal(scores, total, average)
+    
     
 main()
