075dc63e95e82617b3357a8b8ab778d42e7213c2
AmpersandTalks <54295557+AmpersandTalks@users.noreply.github.com>
2021-06-23 02:50:33 -0500

    Update Activity 2.py

--- Assignment 9/Activity 2.py
+++ Assignment 9/Activity 2.py
@@ -1,16 +1,6 @@
 # this program that asks the user to enter grade scores 
 # and calculates the average of the entered scores.
 
-
-def get_scores(total):
-    counter = 1
-    while counter <= total:
-        print(" Enter score value: ")
-        scores = int(input())
-        counter = counter + 1
-      
-    return scores
-
 def get_value(name): 
     print(" Enter " + name + " value: ")
     value = int(input())
@@ -18,15 +8,37 @@ def get_value(name):
     return value
 
 
-def get_total(scores, total, average):
+def get_scores():
+    print(" Enter score value: ")
+    scores = int(input())
+    return scores
+
+
+def process_scores(total):
+    total_scores = 0
+    counter = 1
+    while counter <= total:
+        scores = get_scores()
+        total_scores = total_scores + scores
+        counter = counter + 1
+    return total_scores
+
+
+def calculate_average(total_scores, total):
+    average = float(total_scores) / total
+    return average
+
+
+
+def display_average(average):
     print(" The avergae of all these score is " + str(average))
 
 
 def main():
-    total = get_value("total")
-    scores = get_scores(total)
-    average = float(scores) / total
-    get_total(scores, total, average)
+    total = get_value("scores")
+    total_scores = process_scores(total)
+    average = calculate_average(total_scores, total)
+    display_average(average)
 
 
 main()
