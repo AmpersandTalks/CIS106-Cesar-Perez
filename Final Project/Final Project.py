4c538070691678dfa5667fea1836787c2080e5cc
AmpersandTalks <54295557+AmpersandTalks@users.noreply.github.com>
2021-07-13 23:11:25 -0500

    Update Final Project.py

--- Final Project/Final Project.py
+++ Final Project/Final Project.py
@@ -18,22 +18,71 @@ def get_information():
 def get_title_array(record):
     get_record = record.split("\n")
     data = ""
-    for i in range(0,len(get_record),1):
-        if(get_record[i].find("TITLE") > 0):
-            end  = get_record[i].find("</TITLE>")
-            data = get_record[i][:end]
+    for increment in range(0,len(get_record),1):
+        if(get_record[increment].find("TITLE") > 0):
+            end  = get_record[increment].find("</TITLE>")
+            data = get_record[increment][:end]
     return data
 
-
-
-def get_data_array(main_data):
+def get_data_title(main_data):
     title_data = []
-    for i in range(1,len(main_data)-1,1):
-        title_data.append(get_title_array(main_data[i]))
+    for increment in range(1,len(main_data),1):
+        title_data.append(get_title_array(main_data[increment]))
+    return title_data
+  
+def get_artist_array(record):
+    get_record = record.split("\n")
+    data = ""
+    for increment in range(0,len(get_record),1):
+        if(get_record[increment].find("ARTIST") > 0):
+            start = get_record[i].find(">") + 1
+            end  = get_record[i].find("</ARTIST>")
+            data = get_record[i][start:end]
+    return data
 
-    print(title_data)
-  
-  
+def get_data_artist(main_data):
+    artist_data = []
+    for increment in range(1,len(main_data),1):
+        artist_data.append(get_artist_array(main_data[i]))
+    print(artist_data)
+    return artist_data
+
+
+def get_country_array(record):
+    get_record = record.split("\n")
+    data = ""
+    for increment in range(0,len(get_record),1):
+        if(get_record[increment].find("COUNTRY") > 0):
+            start = get_record[increment].find(">") + 1
+            end  = get_record[increment].find("</COUNTRY>")
+            data = get_record[increment][start:end]
+    return data
+
+def get_data_country(main_data):
+    country_data = []
+    for increment in range(1,len(main_data),1):
+        country_data.append(get_country_array(main_data[increment]))
+    print(country_data)
+    return country_data
+
+def get_company_array(record):
+    get_record = record.split("\n")
+    data = ""
+    for increment in range(0,len(get_record),1):
+        if(get_record[i].find("COMPANY") > 0):
+            start = get_record[increment].find(">") + 1
+            end  = get_record[increment].find("</COMPANY>")
+            data = get_record[increment][start:end]
+    return data
+
+def get_data_artist(main_data):
+    company_data = []
+    for increment in range(1,len(main_data),1):
+        company_data.append(get_artist_array(main_data[increment]))
+    print(artist_data)
+    return artist_data
+
+
 '''catalog_data = []
 for line in cd_data.split("\n"):
     catalog_data.append(line)
@@ -51,45 +100,15 @@ for invrement in range(0,len(catalog_data)-1,1):
     check = catalog_data[increment].find("TITLE")
     if check >= 0:
         title_data.append(catalog_data[increment])
-        print(catalog_data[increment])
-
-for increment in range(0,len(catalog_data)-1,1):
-    check = catalog_data[increment].find("ARTIST")
-    if check >= 0:
-        artist_data.append(catalog_data[i])
-        print(catalog_data[increment])
-
-for increment in range(0,len(catalog_data)-1,1):
-    check = catalog_data[increment].find("COMPANY")
-    if check >= 0:
-        company_data.append(catalog_data[increment])
-        print(catalog_data[increment])
-
-
-for increment in range(0,len(catalog_data)-1,1):
-    check = catalog_data[increment].find("COUNTRY")
-    if check >= 0:
-        country_data.append(catalog_data[increment])
-        print(catalog_data[increment])
-
-
-for increment in range(0,len(catalog_data)-1,1):
-    check = catalog_data[increment].find("PRICE")
-    if check >= 0:
-        price_data.append(catalog_data[increment])
-        print(catalog_data[increment])
-
-for increment in range(0,len(catalog_data)-1,1):
-    check = catalog_data[increment].find("YEAR")
-    if check >= 0:
-        year_data.append(catalog_data[increment])
         print(catalog_data[increment])'''
+
+
 
         
 def main():
     main_data = fetch_info()
-    print(main_data)
-
+    title_data = get_data_title(main_data)
+    artist_data = get_data_artist(main_data)
 
 
 main()
