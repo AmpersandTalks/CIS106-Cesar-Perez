759fedafb45e1d3309b1b6330c95cebf211d01ea
AmpersandTalks <54295557+AmpersandTalks@users.noreply.github.com>
2021-07-13 21:17:03 -0500

    Update Final Project.py

--- Final Project/Final Project.py
+++ Final Project/Final Project.py
@@ -13,10 +13,50 @@ def get_information():
 catalog_data = []
 for line in cd_data.split("\n"):
     catalog_data.append(line)
+   
 
-print(catalog_data)
+title_data = []
+artist_data= []
+company_data = []
+country_data = []
+price_data = []
+year_data = []
+
 
 for invrement in range(0,len(catalog_data)-1,1):
-    check = catalog_data[i].find("TITLE")
+    check = catalog_data[increment].find("TITLE")
     if check >= 0:
-        print(catalog_data[i])
+        title_data.append(catalog_data[increment])
+        print(catalog_data[increment])
+
+for increment in range(0,len(catalog_data)-1,1):
+    check = catalog_data[increment].find("ARTIST")
+    if check >= 0:
+        artist_data.append(catalog_data[i])
+        print(catalog_data[increment])
+
+for increment in range(0,len(catalog_data)-1,1):
+    check = catalog_data[increment].find("COMPANY")
+    if check >= 0:
+        company_data.append(catalog_data[increment])
+        print(catalog_data[increment])
+
+
+for increment in range(0,len(catalog_data)-1,1):
+    check = catalog_data[increment].find("COUNTRY")
+    if check >= 0:
+        country_data.append(catalog_data[increment])
+        print(catalog_data[increment])
+
+
+for increment in range(0,len(catalog_data)-1,1):
+    check = catalog_data[increment].find("PRICE")
+    if check >= 0:
+        price_data.append(catalog_data[increment])
+        print(catalog_data[increment])
+
+for increment in range(0,len(catalog_data)-1,1):
+    check = catalog_data[increment].find("YEAR")
+    if check >= 0:
+        year_data.append(catalog_data[increment])
+        print(catalog_data[increment])
