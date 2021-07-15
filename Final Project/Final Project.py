a73d857b7c36af7d72cc2ef77c12fd7d667eb869
AmpersandTalks <54295557+AmpersandTalks@users.noreply.github.com>
2021-07-15 03:53:05 -0500

    Update Final Project.py

--- Final Project/Final Project.py
+++ Final Project/Final Project.py
@@ -1,22 +1,23 @@
 # This program gets data from a XML file from online 
 # and display in requested way, it also check data validation.
 
-
 import urllib.request
 
+
 def get_information():
-  cd_catalog = "https://www.w3schools.com/xml/cd_catalog.xml"
-  try:
-        cd_data = urllib.request.urlopen(cd_catalog).read().decode()
-        main_data = cd_data.split("<TITLE>")
-  except Exception as exception:
+    breakfast_menu = "https://www.w3schools.com/xml/simple.xml"
+    try:
+        breakfast_menu = urllib.request.urlopen(breakfast_menu).read().decode()
+        main_data = breakfast_menu.split("</NAME>")
+    except Exception as exception:
         print("Try to connect internet or web page not available")
         exit(1)
     return main_data
 
+
 def get_array(record, tag):
     get_record = record.split("\n")
-     data = "None"
+    data = "None"
     try:
         for increment in range(0,len(get_record), 1):
             if(get_record[increment].find(tag) > 0):
@@ -28,23 +29,47 @@ def get_array(record, tag):
     return data
   
 def get_data(main_data, tag):
-    year_data = []
-     for increment in range(0, len(main_data)-1, 1):
-        year_data.append(get_array(main_data[increment], tag))
-    return year_data
+    calories_data = []
+    for increment in range(0, len(main_data)-1, 1):
+        calories_data.append(get_array(main_data[increment], tag))
+    return calories_data
 
 def check_price(price):
     for increment in range(0, len(price), 1):
         try:
             price[increment] = float(price[increment])
         except:
+            print(f"Invalid price data: price[{increment}] = {price[increment]}")
             price[increment] = 0.00
     return price
   
-def display_output(title_data, artist_data, country_data, company_data, price_data, year_data):
-    for increment in range (0, len(title_data), 1):
-        print(f"{title_data[increment]} - {artist_data[increment]} - {country_data[increment]} - {company_data[increment]} - $ {price_data[increment]} - {year_data[increment]}")  
- 
+def display_output(name_data, price_data, description_data, calories_data):
+    length = max(
+            len(name_data), 
+            len(price_data),
+            len(description_data), 
+            len(calories_data))
+
+    if len(name_data) < length:
+        print("Invalid data: missing names")
+        return
+
+    if len(description_data) < length:
+        print("Invalid data: missing descriptions")
+        return
+
+    if len(calories_data) < length:
+        print("Invalid data: missing calories")
+        return
+
+    if len(price_data) < length:
+        print("Invalid data: missing prices")
+        return
+
+    for increment in range(len(name_data)):
+        print(f"{name_data[increment]} - {description_data[increment]} - {calories_data[increment]} - $ {price_data[increment]}")
+
+
 def get_data_average(price):
     total = 0
     for increment in range(0, len(price), 1):
@@ -53,21 +78,19 @@ def get_data_average(price):
     return average
 
 def display_output_average(record, average):
-    print(f" Total Records Available in Catalog : {record}")
-    print(f" Average price of items in Catalog  : {average :.2f}")
+    print(f" Total Records Available in menu : {record}")
+    print(f" Average price of items in menu  : {average :.2f}")
 
 def main():
-    main_data = fetch_info()
-    title_data = get_data(main_data, "</TITLE>")
-    artist_data = get_data(main_data, "</ARTIST>")
-    country_data = get_data(main_data, "</COUNTRY>")
-    company_data = get_data(main_data, "</COMPANY>")
+    main_data = get_information()
+    name_data = get_data(main_data, "</NAME>")
+    description_data = get_data(main_data, "</DESCRIPTION>")
+    calories_data = get_data(main_data, "</CALORIES>")
     price_data = get_data(main_data, "</PRICE>")
     price_data = check_price(price_data)
-    year_data = get_data(main_data, "</YEAR>")
-    print_output(title_data, artist_data, country_data, company_data, price_data, year_data)
+    display_output(name_data, price_data, description_data, calories_data )
     average_data = get_data_average(price_data)
-    print_output_average(len(title_data), average_data)
-    
-   
+    display_output_average(len(name_data), average_data)
+
+
 main()
